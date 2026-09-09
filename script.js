033d895a300d69da4589469f5a0fd600e041d733
Dawson-git <dawsongardels@u.boisestate.edu>
2026-09-09 16:04:47 -0600

    Getting startup down

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 console.log("Js is here!");
 (function() {
 
-    var outputElem = document.getElementById("demo"),  //where to output the letter on the screen
-        current = 0,  //start index in the array
-        randomChars = ["W","E","L","C","O","M","E","!"],  //characters to show
+
+    var outputElem = document.getElementById("demo");  //where to output the letter on the screen
+        current = 0;  //start index in the array
+        console.log("we got here");
+        startupString = "WELCOME... \n ENTER CREDENTIALS>>>";
         timer = window.setInterval(  //this is how we will loop with an interval
             function () {
-                var letter = randomChars[current];  //get next letter
+                var letter = startupString[current];  //get next letter
                 if (letter) {  //if there is no letter, it will be undefined and we will be done
-                    outputElem.innerHTML = letter;  //show the letter to the user
+                    outputElem.innerHTML += letter;  //show the letter to the user
                     current++;  //update the index
+                    console.log("in the loop");
                 } else {
                     window.clearInterval(timer);  //cancel the timer since we ran out of things to display
                 }
             }
-        ,1000);  //number of seconds to wait between iterations
+        ,25);  //number of seconds to wait between iterations
 
 }());
